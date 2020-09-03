8e0a419554b0e7e444d53dd93ac105e070230db0
Wohlstand <admin@wohlnet.ru>
2020-09-03 13:45:14 +0300

    Build script

---
+++ build.bat
@@ -0,0 +1,10 @@
+@echo off
+
+D:
+
+msmake ail.mak
+
+if errorlevel 1 pause
+
+exit
+
---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ "$1" == "clean" ]]; then
+	for s in *.adv *.obj tmp.xmi *.drv *.exe; do
+		echo "$s"
+		if [[ "$s" != "msmake.exe" && -f "$s" ]]; then
+			rm -v $s
+		fi
+
+	done
+	exit 0;
+fi
+
+dosbox -conf dosbox/dosbox.conf -c @D:\build.bat
+
+for SRC in *
+do
+    DST=`dirname "${SRC}"`/`basename "${SRC}" | tr '[A-Z]' '[a-z]'`
+    if [ "${SRC}" != "${DST}" ]
+    then
+        [ ! -e "${DST}" ] && mv -T "${SRC}" "${DST}" || echo "${SRC} was not renamed"
+    fi
+done
+
